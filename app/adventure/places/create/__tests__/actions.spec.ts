be3d6a5100ae5cfc7af41795c558ae9c46732bc9
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-22 18:27:34 -0500

    add tests for the places data layer and actions

---
+++ app/adventure/places/create/__tests__/actions.spec.ts
@@ -0,0 +1,42 @@
+import { Place } from '@/models';
+import { PLACES } from '../../__mocks__/data';
+import { createConfirmed } from '../actions';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { addPlace } from '../../data';
+import { redirect } from 'next/navigation';
+
+vi.mock('../../data');
+vi.mock('next/navigation');
+
+const place: Place = { ...PLACES.find((p) => p.id === 2)!, id: undefined };
+
+describe('places: createConfirmed', () => {
+  beforeEach(() => vi.clearAllMocks());
+
+  it('calls addPlace with the specified place', async () => {
+    await createConfirmed(place);
+    expect(addPlace).toHaveBeenCalledExactlyOnceWith(place);
+  });
+
+  describe('on success', () => {
+    beforeEach(() => {
+      (addPlace as any).mockResolvedValue({ ...place, id: 73 });
+    });
+
+    it('redirects to the places page', async () => {
+      await createConfirmed(place);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/adventure/places');
+    });
+  });
+
+  describe('on failure', () => {
+    beforeEach(() => {
+      (addPlace as any).mockResolvedValue(null);
+    });
+
+    it('redirects to the error page', async () => {
+      await createConfirmed(place);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/error');
+    });
+  });
+});
