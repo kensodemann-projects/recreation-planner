347417f1ca92b469f7f305eb300cd5d7673d37c9
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-23 04:06:24 -0500

    add tests for the places data layer and actions

---
+++ app/adventure/places/[id]/notes/create/__tests__/actions.spec.ts
@@ -0,0 +1,42 @@
+import { NOTES } from '@/app/adventure/notes/__mocks__/data';
+import { Note } from '@/models';
+import { describe, beforeEach, vi, it, expect } from 'vitest';
+import { createConfirmed } from '../actions';
+import { addNote } from '@/app/adventure/notes/data';
+import { redirect } from 'next/navigation';
+
+vi.mock('@/app/adventure/notes/data');
+vi.mock('next/navigation');
+
+const note: Note = { ...NOTES.find((n) => n.placeRid !== null)!, id: undefined, placeRid: 19 };
+
+describe('place notes: createConfirmed', () => {
+  beforeEach(() => vi.clearAllMocks());
+
+  it('calls addNote with the specified note', async () => {
+    await createConfirmed(note);
+    expect(addNote).toHaveBeenCalledExactlyOnceWith(note);
+  });
+
+  describe('when addNote succeeds', () => {
+    beforeEach(() => {
+      (addNote as any).mockResolvedValue({ ...note, id: 73 });
+    });
+
+    it('redirects to the place details page', async () => {
+      await createConfirmed(note);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/adventure/places/19');
+    });
+  });
+
+  describe('when addNote fails', () => {
+    beforeEach(() => {
+      (addNote as any).mockResolvedValue(null);
+    });
+
+    it('redirects to /error', async () => {
+      await createConfirmed(note);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/error');
+    });
+  });
+});
