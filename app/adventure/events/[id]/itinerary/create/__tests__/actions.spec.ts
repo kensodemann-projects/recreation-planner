e468d932ab1680fd815c9425922f3d27b3dd35a1
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-23 10:29:25 -0500

    add tests for the itinerary data layer and actions

---
+++ app/adventure/events/[id]/itinerary/create/__tests__/actions.spec.ts
@@ -0,0 +1,41 @@
+import { ITINERARY_ITEMS } from '@/app/adventure/events/[id]/itinerary/__mocks__/data';
+import { addItineraryItem } from '@/app/adventure/events/[id]/itinerary/data';
+import { ItineraryItem } from '@/models';
+import { redirect } from 'next/navigation';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { createConfirmed } from '../actions';
+
+vi.mock('@/app/adventure/events/[id]/itinerary/data');
+vi.mock('next/navigation');
+
+const itineraryItem: ItineraryItem = { ...ITINERARY_ITEMS.find((i) => i.id === 3)!, id: undefined, eventRid: 332 };
+
+describe('itinerary: createConfirmed', () => {
+  beforeEach(() => vi.clearAllMocks());
+  it('calls addItineraryItem with the specified itinerary item', async () => {
+    await createConfirmed(itineraryItem);
+    expect(addItineraryItem).toHaveBeenCalledExactlyOnceWith(itineraryItem);
+  });
+
+  describe('when addItineraryItem succeeds', () => {
+    beforeEach(() => {
+      (addItineraryItem as any).mockResolvedValue({ ...itineraryItem, id: 73 });
+    });
+
+    it('redirects to the event details page', async () => {
+      await createConfirmed(itineraryItem);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/adventure/events/332?lastActivity=Itinerary');
+    });
+  });
+
+  describe('when addItineraryItem fails', () => {
+    beforeEach(() => {
+      (addItineraryItem as any).mockResolvedValue(null);
+    });
+
+    it('redirects to /error', async () => {
+      await createConfirmed(itineraryItem);
+      expect(redirect).toHaveBeenCalledExactlyOnceWith('/error');
+    });
+  });
+});
