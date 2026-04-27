03dc53d31abd8b4bad8d4404346caacd7d6b93b6
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-27 10:16:40 -0500

    WIP - create withAuth

--- utils/supabase/auth.ts
+++ utils/supabase/auth.ts
@@ -1,3 +1,4 @@
+import { SupabaseClient } from '@supabase/supabase-js';
 import { createClient } from './server';
 
 export const isNotLoggedIn = async (): Promise<boolean> => {
@@ -8,4 +9,16 @@ export const isNotLoggedIn = async (): Promise<boolean> => {
   } = await supabase.auth.getUser();
 
   return !user;
+};
+
+export const withAuth = <TArgs extends unknown[], TResult>(
+  fn: (supabase: SupabaseClient, ...args: TArgs) => Promise<TResult>,
+): ((...args: TArgs) => Promise<TResult | null>) => {
+  return async (...args) => {
+    const supabase = createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    return user ? fn(supabase, ...args) : null;
+  };
 };
