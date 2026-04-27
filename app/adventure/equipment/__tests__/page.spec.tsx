6a6d1a422315293f65fbad29cf9eaef2d5a57f54
Ken Sodemann <ken.sodemann@gmail.com>
2026-04-27 17:44:18 -0500

    centralize the auth checking to the middleware proxy

--- app/adventure/equipment/__tests__/page.spec.tsx
+++ app/adventure/equipment/__tests__/page.spec.tsx
@@ -1,6 +1,5 @@
-import { isNotLoggedIn } from '@/utils/supabase/auth';
 import { cleanup, render, screen } from '@testing-library/react';
-import { afterEach, beforeEach, describe, expect, it, Mock, vi } from 'vitest';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
 import { fetchAllEquipment } from '../data';
 import EquipmentPage from '../page';
 
@@ -11,51 +10,21 @@ describe('Equipment Page', () => {
   afterEach(() => cleanup());
   beforeEach(() => vi.clearAllMocks());
 
-  describe('when logged in', () => {
-    beforeEach(() => {
-      (isNotLoggedIn as Mock).mockResolvedValue(false);
-    });
-
-    it('fetches the equipment', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(fetchAllEquipment).toHaveBeenCalledExactlyOnceWith();
-    });
-
-    it('renders the equipment component', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(screen.getByRole('heading', { level: 1, name: 'Equipment' })).toBeDefined();
-    });
-
-    it('does not render the must be logged in component', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(screen.queryByRole('heading', { level: 1, name: 'You must be logged in' })).toBeNull();
-    });
+  it('fetches the equipment', async () => {
+    const jsx = await EquipmentPage();
+    render(jsx);
+    expect(fetchAllEquipment).toHaveBeenCalledExactlyOnceWith();
   });
 
-  describe('when not logged in', () => {
-    beforeEach(() => {
-      (isNotLoggedIn as Mock).mockResolvedValue(true);
-    });
+  it('renders the equipment component', async () => {
+    const jsx = await EquipmentPage();
+    render(jsx);
+    expect(screen.getByRole('heading', { level: 1, name: 'Equipment' })).toBeDefined();
+  });
 
-    it('renders the must be logged in component', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(screen.getByRole('heading', { level: 1, name: 'You must be logged in' })).toBeDefined();
-    });
-
-    it('does not fetch the equipment', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(fetchAllEquipment).not.toHaveBeenCalled();
-    });
-
-    it('does not render the equipment component', async () => {
-      const jsx = await EquipmentPage();
-      render(jsx);
-      expect(screen.queryByRole('heading', { level: 1, name: 'Equipment' })).toBeNull();
-    });
+  it('does not render the must be logged in component', async () => {
+    const jsx = await EquipmentPage();
+    render(jsx);
+    expect(screen.queryByRole('heading', { level: 1, name: 'You must be logged in' })).toBeNull();
   });
 });
--- proxy.ts
+++ proxy.ts
@@ -1,8 +1,23 @@
-import { type NextRequest } from 'next/server';
+import { NextResponse, type NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/proxy';
+import { createClient } from './utils/supabase/server';
 
 export async function proxy(request: NextRequest) {
-  return await updateSession(request);
+  const res = await updateSession(request);
+
+  if (request.nextUrl.pathname.startsWith('/adventure/')) {
+    const supabase = createClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (!user) {
+      const url = request.nextUrl.clone();
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return res;
 }
 
 export const config = {
