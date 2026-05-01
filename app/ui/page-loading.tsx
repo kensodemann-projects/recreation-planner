b2a8e1409342d6ed7b2a0298394d802599543ec3
Ken Sodemann <ken.sodemann@gmail.com>
2026-05-01 15:29:37 -0500

    WIP - dead code

--- app/ui/page-loading.tsx
+++
@@ -1,9 +0,0 @@
-const PageLoading = () => {
-  return (
-    <div className="text-center mt-32">
-      <span className="loading loading-dots loading-lg"></span>
-    </div>
-  );
-};
-
-export default PageLoading;
