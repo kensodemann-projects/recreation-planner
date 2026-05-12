b46755da68be358a998c4df06529614207488b31
Ken Sodemann <ken.sodemann@gmail.com>
2026-05-12 04:26:04 -0500

    WIP - display the title

--- app/adventure/events/ui/__tests__/event-card.spec.tsx
+++ app/adventure/events/ui/__tests__/event-card.spec.tsx
@@ -1,11 +1,28 @@
-import { cleanup } from '@testing-library/react';
+import { PLACES } from '@/app/adventure/places/__mocks__/data';
+import { Event } from '@/models';
+import { cleanup, render, screen } from '@testing-library/react';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { EVENT_TYPES } from '../../__mocks__/data';
+import EventCard from '../event-card';
 
 describe('Event Card', () => {
   beforeEach(() => vi.clearAllMocks());
   afterEach(() => cleanup());
 
-  it('is a placeholder', () => {
-    expect(true).toBe(true);
+  it('renders the title', () => {
+    render(<EventCard event={TEST_EVENT} />);
+    expect(screen.getByRole('heading', { level: 3, name: TEST_EVENT.name })).toBeDefined();
   });
 });
+
+const TEST_EVENT: Event = {
+  id: 314,
+  beginDate: '2024-09-28',
+  beginTime: '18:30',
+  endDate: '2024-09-30',
+  endTime: '22:30',
+  name: "Women's Hockey Tournament",
+  description: 'A three day long tournament of women playing hockey',
+  place: PLACES[3],
+  type: EVENT_TYPES[1],
+};
--- app/adventure/events/ui/event-card.tsx
+++ app/adventure/events/ui/event-card.tsx
@@ -1,7 +1,18 @@
 import { Event } from '@/models';
 
 export interface EventCardProps {
-  baseHref: string;
   event: Event;
   callingPage?: string;
 }
+
+const EventCard = ({ event }: EventCardProps) => {
+  return (
+    <div className="card card-border bg-base-100">
+      <div className="card-body">
+        <h3 className="card-title">{event.name}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default EventCard;
