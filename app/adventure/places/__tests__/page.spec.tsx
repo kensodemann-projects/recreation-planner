4a514a5ee75746c86c50e28f61b10bed4baed1c9
Ken Sodemann <ken.sodemann@gmail.com>
2026-05-23 07:30:36 -0400

    filter the places based on type

--- app/adventure/places/__tests__/page.spec.tsx
+++ app/adventure/places/__tests__/page.spec.tsx
@@ -1,6 +1,6 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
-import { fetchPlaces } from '../data';
+import { fetchPlaces, fetchPlaceTypes } from '../data';
 import PlacesPage from '../page';
 
 vi.mock('../data');
@@ -15,6 +15,11 @@ describe('Places Page', () => {
   it('fetches the places', async () => {
     await PlacesPage();
     expect(fetchPlaces).toHaveBeenCalledExactlyOnceWith();
+  });
+
+  it('fetches the place types', async () => {
+    await PlacesPage();
+    expect(fetchPlaceTypes).toHaveBeenCalledExactlyOnceWith();
   });
 
   it('renders the places component', async () => {
